2adc7d9fd62da2f8d46a1de6cea6524f2420d0ba
Partha Sarathi Praharaj <parthasarathi6686@gmail.com>
2025-01-24 14:00:56 +0530

    Initial Commit

---
+++ WaveflowTests/WaveflowTests.swift
@@ -0,0 +1,17 @@
+//
+//  WaveflowTests.swift
+//  WaveflowTests
+//
+//  Created by Partha Praharaj on 24/01/25.
+//
+
+import Testing
+@testable import Waveflow
+
+struct WaveflowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
